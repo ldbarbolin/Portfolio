ba0de877f1062dcbdc857e1ec1bdeb8237296a1f
ldbarbolin <ldbarbolin@gmail.com>
2025-05-29 09:16:24 -0400

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/INZA-Portfolio/',
   plugins: [tailwindcss(),react()],
-  base: '/portfolio/',
+  
 })
